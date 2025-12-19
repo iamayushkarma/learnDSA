e9fa72d95fd3bafce24040d99b867ca0501e2ef4
Ayush Karma <ayushkarma.dev@gmail.com>
2025-12-19 11:34:21 +0530

    Add questions in Binary search

--- Searching/BinarySearchProblem.java
+++ Searching/BinarySearchProblem.java
@@ -50,6 +50,60 @@ public class BinarySearchProblem {
         return ans;
     }
 
+    // > Q3
+    // A rotated sorted array is a sorted array on which rotation operation has been
+    // performed some number of times. Given a rotated sorted array, find the index
+    // of the minimum element in the array. Follow 0-based indexing. It is
+    // guaranteed that all the elements in the array are unique.
+
+    static int findMinimum(int[] arr) {
+        int ans = -1;
+        int n = arr.length;
+        int i = 0, j = n - 1;
+
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (arr[mid] <= arr[n - 1]) {
+                ans = mid;
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // > Q4
+    // Given the rotated sorted array of integers, which contains distinct elements,
+    // and an integer target, return the index of target ifit is in the array.
+    // Otherwise return -1.
+
+    static int searchForNumber(int[] arr, int target) {
+        int ans = -1;
+        int n = arr.length;
+        int i = 0, j = n - 1;
+
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (arr[mid] == target)
+                return mid;
+            else if (arr[mid] < arr[j]) {
+                if (target > arr[mid] && target <= arr[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            } else {
+                if (target >= arr[i] && target < arr[mid]) {
+                    j = mid - 1;
+                } else {
+                    i = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
         // > Q1
@@ -59,5 +113,13 @@ public class BinarySearchProblem {
         // > Q2
         int x = 25;
         System.out.println(sqrt(x));
+
+        // > Q3
+        int[] arr3 = { 3, 4, 5, 6, 7, 8, 1, 2 };
+        System.out.println("Minimum element is at index: " + findMinimum(arr3));
+
+        // > Q4
+        int[] arr4 = { 3, 4, 5, 6, 7, 8, 1, 2 };
+        System.out.println("Target element is at index: " + searchForNumber(arr4, 7));
     }
 }
